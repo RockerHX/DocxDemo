59a37b9916a6e7fd337c5354940d5d761c2f744b
RockerHX <rockerhx@gmail.com>
2019-05-13 18:28:27 +0800

    Update[Code]: Load docx
    完成读取docx的示例代码：docx实际上是zip压缩包，解压之后通过解析其中的xml来读取内容。

--- DocxDemo/AppDelegate.swift
+++ DocxDemo/AppDelegate.swift
@@ -19,28 +19,80 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+}
+
+
+extension String {
+
+    func delete(prefix: String) -> String {
+        guard self.hasPrefix(prefix) else { return self }
+        return String(self.dropFirst(prefix.count))
     }
 
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    func delete(suffix: String) -> String {
+        guard self.contains(suffix) else { return self }
+        return String(self.dropLast(suffix.count))
     }
 
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    func delete(prefix: String, suffix: String) -> String {
+        let tmp = self.delete(prefix: prefix)
+        return tmp.delete(suffix: suffix)
     }
 
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
+}
 
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
 
+import Files
+import Zip
+import AEXML
+
+extension AppDelegate {
+
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        let fileAgreement = "file://"
+        let docSuffix = ".docx"
+        let zipSuffix = ".zip"
+        if url.absoluteString.contains(fileAgreement) {
+            var fileURL = url
+            let fileNameSuffix = fileURL.lastPathComponent
+            let fileName = fileNameSuffix.delete(suffix: docSuffix)
+            // 使用Folder库初始化的路径不能带协议，不能带文件名
+            fileURL.deleteLastPathComponent()
+            let filePath = fileURL.absoluteString.delete(prefix: fileAgreement)
+            do {
+                // 先清空临时文件夹
+                try Folder.temporary.files.forEach { (file) in
+                    try file.delete()
+                }
+                try Folder.temporary.subfolders.forEach { (folder) in
+                    try folder.delete()
+                }
+                // 操作临时文件
+                let fileFolder = try Folder(path: filePath)
+                let file = try fileFolder.file(named: fileNameSuffix)
+                let tmpFile = try file.copy(to: Folder.temporary)
+                try tmpFile.rename(to: fileName + zipSuffix, keepExtension: false)
+                // 初始化解压路径
+                guard let utf8 = "\(fileAgreement)\(tmpFile.path)".addingPercentEncoding(withAllowedCharacters: .urlFragmentAllowed),
+                      let source = URL(string: utf8),
+                      let target = URL(string: fileAgreement + Folder.temporary.path)
+                else { return false}
+                // 解压操作
+                try Zip.unzipFile(source, destination: target, overwrite: true, password: nil, progress: { (progress) -> () in
+                    print(progress)
+                })
+                // 读取xml
+                let targetFile = try Folder.temporary.subfolder(named: "word").file(named: "document.xml")
+                guard let targetURL = URL(string: fileAgreement + targetFile.path) else { return false }
+                let data = try Data(contentsOf: targetURL)
+                let xmlDoc = try AEXMLDocument(xml: data)
+                debugPrint(xmlDoc.xml)
+            } catch {
+                debugPrint(error)
+            }
+        }
+        return true
+    }
 
 }
 
